e2c12a81f5aaccb5acfef0826dcb29b2fcf6a979
Wixonic <public.wixonic@icloud.com>
2022-07-10 11:54:40 +0200

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,25 @@
+window.executeDOMActions = (actions) => {
+	for (let action of actions) {
+		const elements = document.querySelectorAll(action.selector);
+		
+		for (let element of elements) {
+			for (let event of action.events) {
+				element.addEventListener(event,action);
+			}
+		}
+	}
+};
+
 window.addEventListener("load",() => {
-	
+	const actions = [
+		{
+			selector: "*",
+			events: ["click","mousedown","mousemove","mouseup","touchstart","touchmove","touchend","input"],
+			function: (event) => {
+				if (this.getAttribute("disabled") == "true") {
+					event.preventDefault();
+				}
+			}
+		}
+	];
 });
