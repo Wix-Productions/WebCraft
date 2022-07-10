7b0f99ebc35936bf236283719fb0eb88a2ff7a47
Wixonic <public.wixonic@icloud.com>
2022-07-10 11:38:01 +0200

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,3 @@
 window.addEventListener("load",() => {
-	for (let interface of Interfaces) {
-		if (interface.status == "down") {
-			for (let element of document.getElementsByClassName(`interface-${interface}`)) {
-				element.setAttribute("disabled","true");
-			}
-		}
-	}
+	
 });
