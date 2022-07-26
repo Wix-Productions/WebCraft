e624a4839d73ba060c08ecb8d57c6cecd7f53a2d
Wixonic <public.wixonic@icloud.com>
2022-07-27 00:00:31 +0200

    Update utils.js

--- core/utils.js
+++ core/utils.js
@@ -1,5 +1,5 @@
 const crash = (info="Oops",reason="An unknow error occured",errorcode="0") => {
-	main.innerHTML = `<crash><info>${cause}</info><reason>${reason}</reason><a href="https://github.com/Wix-Productions/WebCraft/wiki/Error-Codes/${errorcode}">Error code: ${errorcode}</a></crash>`;
+	main.innerHTML = `<crash><info>${cause}</info><reason>${reason}</reason><a href="https://github.com/Wix-Productions/WebCraft/wiki/Error-Code%3A-${errorcode}">Error code: ${errorcode}</a></crash>`;
 };
 
 const decodeDataURL = (url) => {
